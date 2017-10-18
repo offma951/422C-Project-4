0b8f34dc855091804930a140059164ba4e0c7eba
offma951 <32873838+offma951@users.noreply.github.com>
2017-10-18 18:00:53 -0500

    Project Created

---
+++ src/assignment4/InvalidCritterException.java
@@ -0,0 +1,16 @@
+package assignment4;
+
+
+public class InvalidCritterException extends Exception {
+	private static final long serialVersionUID = 1L;
+	String offending_class;
+	
+	public InvalidCritterException(String critter_class_name) {
+		offending_class = critter_class_name;
+	}
+	
+	public String toString() {
+		return "Invalid Critter Class: " + offending_class;
+	}
+
+}
